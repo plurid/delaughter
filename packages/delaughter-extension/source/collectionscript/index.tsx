a83603af54784aac10be5cf5dbb1abde4496328b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-04-26 10:12:22 +0300

    delaughter-extension > dev: collectionscript

---
+++ packages/delaughter-extension/source/collectionscript/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import CollectionScript from './CollectionScript';
+
+
+
+const app = document.createElement('div');
+document.body.append(app);
+const root = createRoot(app);
+
+root.render(
+    <React.StrictMode>
+        <CollectionScript />
+    </React.StrictMode>
+);
